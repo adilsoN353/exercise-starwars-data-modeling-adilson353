e22728e3072a16747e8c25fb2a6843169cdc767e
adilson aguilar <163350523+adilsoN353@users.noreply.github.com>
2024-07-19 18:38:04 +0000

    terminado

--- src/models.py
+++ src/models.py
@@ -1,30 +1,83 @@
 import os
 import sys
-from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy import Column, ForeignKey, Integer, String, Boolean
 from sqlalchemy.orm import relationship, declarative_base
 from sqlalchemy import create_engine
 from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
+class User(Base):
+    __tablename__ = 'user'
+    
     # Here we define columns for the table person
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+    name = Column(String(250), nullable=True)
+    # First_name = Column(String(250), nullable=True)
+    Last_name = Column(String(250), nullable=True)
+    user_name = Column(String(250), nullable= True)
+    email = Column(String(300), nullable=True)
+    is_active = Column(Boolean(),unique = False , nullable = False)
+    favorites = relationship ("Favorites", backref = "user", lazy = True) 
 
-class Address(Base):
-    __tablename__ = 'address'
+    
+
+class Planet(Base):
+    __tablename__ = 'planet'
     # Here we define columns for the table address.
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    name = Column(String(250), nullable=True)
+    favorites = relationship ("Favorites", backref = "user", lazy = True)
 
+
+    diameter = Column(String(250), nullable = False) 
+    rotation_period = Column(String(250), nullable = False) 
+    orbital_period = Column(String(250), nullable = False) 
+    gravity = Column(String(250), nullable = False) 
+    population = Column(String(250), nullable = False) 
+    climate = Column(String(250), nullable = False) 
+
+
+class Favorites(Base):
+    __tablename__ = 'favorites'
+    id = Column(Integer, primary_key=True)
+    planet_id = Column(Integer , ForeignKey("planet.id"))
+    starships_id = Column(Integer , ForeignKey("starships.id"))
+    characters_id = Column(Integer , ForeignKey("characters.id"))
+    user_id = Column(Integer, ForeignKey("user.id"))
+
+class Starships(Base):
+    __tablename__ = 'starships'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=True)
+    favorites = relationship ("Favorites", backref = "user", lazy = True)
+
+    model = Column(String(250), nullable = False) 
+    starship_class = Column(String(250), nullable = False) 
+    manufacturer = Column(String(250), nullable = False) 
+    cost_in_credits = Column(String(250), nullable = False) 
+    length = Column(String(250), nullable = False) 
+    crew = Column(String(250), nullable = False) 
+    passengers = Column(String(250), nullable = False) 
+
+
+
+class Characters(Base):
+    __tablename__ = 'characters'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=True)
+    favorites = relationship ("Favorites", backref = "user", lazy = True)
+
+    height =  Column(String(250), nullable = False) 
+    mass =  Column(String(250), nullable = False) 
+    hair_color =  Column(String(250), nullable = False) 
+    skin_color =  Column(String(250), nullable = False) 
+    eye_color =  Column(String(250), nullable = False) 
+    birth_year =  Column(String(250), nullable = False) 
     def to_dict(self):
         return {}
 
